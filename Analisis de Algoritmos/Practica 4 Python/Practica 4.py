b6e169b166fa1698e14d37d82a00969e57e1ff07
Sebastian Martinez <sebastian.martinez.lopez98@gmail.com>
2022-12-21 13:17:39 -0600

    Update Practica 4.py

--- Analisis de Algoritmos/Practica 4 Python/Practica 4.py
+++ Analisis de Algoritmos/Practica 4 Python/Practica 4.py
@@ -2,15 +2,31 @@ import cv2 #Para el procesamiento de la imagen
 import numpy as np #Para la creacion y manipulacion de matrices
 import pandas as pd #Para operaciones con matrices
 
-def divideyvenceras(lista):
-    tamaño=len(lista)
-    print(lista)
-    if(tamaño!=1):
-        validar=int(tamaño/2)
-        aux=lista[0:validar]
-        divideyvenceras(aux)
+def divideyvenceras(matriz,tamaño):
+    if(tamaño!=2):
+        partir=int(tamaño/2)
+        A= matriz[0:partir,0:partir]
+        print(A)
+        B= matriz[0:partir,partir:tamaño]
+        print(B)
+        C= matriz[partir:tamaño,0:partir]
+        print(C)
+        D= matriz[partir:tamaño,partir:tamaño]
+        print(D)
+        divideyvenceras(A,int(len(A)))
+
+
     else:
-        nuevo_r.append(lista[0])
+        A=matriz[0:1,0:1]
+        B=matriz[0:1,1:2]
+        C=matriz[1:2,0:1]
+        D=matriz[1:2,1:2]
+        print(A[0])
+
+
+
+
+    
 
 
 
@@ -22,17 +38,23 @@ def divideyvenceras(lista):
 
 
 
-Imagen=cv2.imread('imagen.bmp') #Se carga la imagen
-R=Imagen[:,:,0] #Se descompone la primer capa en Rojo
-G=Imagen[:,:,1] #Se descompone la primer capa en Verde
-B=Imagen[:,:,2] #Se descompone la primer capa en Azul
-cv2.imshow('RGB',np.hstack([R,G,B])) #Se muestra la descomposicion de las image
-cv2.waitKey(0)
-nuevo_r=[]
-for i in R:
-    divideyvenceras(i)
-print(nuevo_r)
-print(len(nuevo_r))
-pd.DataFrame(R).to_excel("R.xlsx") #Se escribe en un excel
-pd.DataFrame(G).to_excel("G.xlsx")
-pd.DataFrame(B).to_excel("B.xlsx")
+
+
+
+
+
+
+
+
+#Imagen=cv2.imread('imagen.bmp') #Se carga la imagen
+#R=Imagen[:,:,0] #Se descompone la primer capa en Rojo
+#G=Imagen[:,:,1] #Se descompone la primer capa en Verde
+#B=Imagen[:,:,2] #Se descompone la primer capa en Azul
+auxa=[]
+#cv2.imshow('RGB',np.hstack([R,G,B])) #Se muestra la descomposicion de las image
+prueba=[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+prueba=np.array(prueba)
+
+divideyvenceras(prueba,int(len(prueba)))
+#divideyvenceras(R,int(len(R)))
+#cv2.waitKey(0)
