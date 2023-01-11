0a9ea605758a829913648d4140713ca1c90216be
Sebastian Martinez <sebastian.martinez.lopez98@gmail.com>
2023-01-11 15:51:41 -0600

    FUNCIONA ARISTAS

--- Analisis de Algoritmos/Punto extra/puntoextra.py
+++ Analisis de Algoritmos/Punto extra/puntoextra.py
@@ -8,15 +8,19 @@ import ast
 n=int(input("Ingresa el tamaño del nodo: \n"))
 nodos=[]
 aristas=[]
+aristas2=[]
 
 for i in range(n):
     nodos.append(input("Ingresa el nodo: {}\n".format(i+1)))
-    aristas.append(ast.literal_eval(input("Ingresa la arista del nodo {} en forma '(nodo,arista)'\n".format(i+1))))
-
+    #aristas.append(ast.literal_eval(input("Ingresa la arista del nodo {} en forma '(nodo,arista)'\n".format(i+1))))
+    aristas.append(tuple(input("Ingresar la arista del nodo {} en forma 'ABC.....'\n").format(i+1)))
 grafo=nx.Graph() #Se crea el nodo
 
 for i in nodos:
     grafo.add_node(i)
 
-for i in aristas:
-    grafo.add_edge(i)
+
+grafo.add_edges_from(aristas)
+
+print(aristas)
+print(grafo.edges)
